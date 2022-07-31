a1a8249bb39e96d4793fa86ae96593cd72760823
NallaSandeep <nallasandeep056@gmail.com>
2022-08-01 02:33:40 +0530

    init

---
+++ src/test/java/com/deeplearning/springwebsockets/SpringWebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deeplearning.springwebsockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
